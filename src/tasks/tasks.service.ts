5faa86417cc28cc590f0802090edfe9e21943fd0
unknown <kennedyhillary6@gmail.com>
2023-03-22 16:35:16 +0100

    tasks service commit

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TasksStatus } from './tasks.model';
+import * as uuid from 'uuid';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+  createTask(title: string, description: string) {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TasksStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+}
