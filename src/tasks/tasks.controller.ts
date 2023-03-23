6081d8cef8484fc88d85f4ff222421537185f673
unknown <kennedyhillary6@gmail.com>
2023-03-23 11:40:02 +0100

    tasks service commit

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { Body, Post } from '@nestjs/common/decorators';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -8,5 +9,13 @@ export class TasksController {
   @Get()
   getAllTask() {
     this.taskService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ) {
+    console.log('body');
   }
 }
