42bee36aea278a72e1f2e08d884b2e340348448d
unknown <kennedyhillary6@gmail.com>
2023-03-23 12:04:48 +0100

    nodemon config

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { Body, Post } from '@nestjs/common/decorators';
+import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -7,15 +8,15 @@ export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  getAllTask() {
-    this.taskService.getAllTasks();
+  getAllTask(): Task[] {
+    return this.taskService.getAllTasks();
   }
 
   @Post()
   createTask(
     @Body('title') title: string,
     @Body('description') description: string,
-  ) {
-    console.log('body');
+  ): Task {
+    return this.taskService.createTask(title, description);
   }
 }
