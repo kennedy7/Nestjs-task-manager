19e58230fc92b1ae5359bc1ddddd112d50095630
unknown <kennedyhillary6@gmail.com>
2023-03-25 13:00:21 +0100

    validation filterdto

--- src/tasks/dto/get-task-filter.dto.ts
+++ src/tasks/dto/get-task-filter.dto.ts
@@ -1,10 +1,12 @@
-import { IsOptional } from 'class-validator';
+import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 import { TasksStatus } from '../tasks.model';
 
 export class GetTaskFilterDto {
   @IsOptional()
+  @IsIn([TasksStatus.OPEN, TasksStatus.IN_PROGRESS, TasksStatus.DONE])
   status: TasksStatus;
 
   @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
