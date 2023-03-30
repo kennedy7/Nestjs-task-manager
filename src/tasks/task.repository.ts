a1d3be73ee4a1f612050f498918d785adb41b1b6
unknown <kennedyhillary6@gmail.com>
2023-03-30 14:37:48 +0100

    getTask with querybuilder

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -10,9 +10,13 @@ export class TaskRepository extends Repository<Task> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
     if (status) {
-      query.andWhere('task.service = :status', { status });
+      query.andWhere('task.status = :status', { status });
     }
     if (search) {
+      query.andWhere(
+        '(task.title LIKE :search OR task.description LIKE :search)',
+        { search: `%${search}%` },
+      );
     }
     const tasks = await query.getMany();
     return tasks;
