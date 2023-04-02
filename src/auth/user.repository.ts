3d7cea563cebef40ff4b1f50e5ec8235b6558afe
unknown <kennedyhillary6@gmail.com>
2023-04-02 20:42:41 +0100

    ConflictException

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -6,9 +10,20 @@ import { User } from './user.entity';
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
+
     const user = new User();
     user.username = username;
     user.password = password;
-    await user.save();
+
+    try {
+      await user.save();
+    } catch (err) {
+      if (err.code === '23505') {
+        //duplicate username
+        throw new ConflictException('Username already taken');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
