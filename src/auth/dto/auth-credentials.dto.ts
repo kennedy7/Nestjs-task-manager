e39e069e0b76f4751111acb8b2935801a5ca8465
unknown <kennedyhillary6@gmail.com>
2023-04-02 10:22:18 +0100

    password validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,10 @@
-import { IsString, MaxLength, MinLength, minLength } from 'class-validator';
+import {
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+  minLength,
+} from 'class-validator';
 import { Unique } from 'typeorm';
 
 export class AuthCredentialsDto {
@@ -7,7 +13,13 @@ export class AuthCredentialsDto {
   @MaxLength(15)
   username: string;
 
+  @IsString()
   @MinLength(7)
   @MaxLength(30)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'password must contain an UpperCase, Lowercase, Number and/or a Special character',
+  })
+  //   (/((?=.*\d) | (?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
   password: string;
 }
