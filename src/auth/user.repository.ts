cb8734de7c51b81594ab539d6c451d618bc0ee93
unknown <kennedyhillary6@gmail.com>
2023-04-04 15:24:16 +0100

    signInUser

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -32,8 +32,12 @@ export class UserRepository extends Repository<User> {
     authCredentialsDto: AuthCredentialsDto,
   ): Promise<string> {
     const { username, password } = authCredentialsDto;
-    const user = this.findOne({ username });
-    return;
+    const user = await User.findOne({ username });
+    if (user && (await user.validatePassword(password))) {
+      return user.username;
+    } else {
+      return null;
+    }
   }
   private async hashedPassword(
     password: string,
