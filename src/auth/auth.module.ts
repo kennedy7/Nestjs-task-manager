baf34933f855b31a3737e939143a96f78c48de3e
unknown <kennedyhillary6@gmail.com>
2023-04-05 11:22:23 +0100

    jwtmodule-register

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,14 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { UserRepository } from './user.repository';
-
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    PassportModule.register({}),
+    JwtModule.register({}),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
