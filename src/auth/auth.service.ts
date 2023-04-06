67e2159cc266cb87338365febba04200e931b899
unknown <kennedyhillary6@gmail.com>
2023-04-06 10:36:51 +0100

    jwtStrategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,11 +3,13 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
 import { UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
