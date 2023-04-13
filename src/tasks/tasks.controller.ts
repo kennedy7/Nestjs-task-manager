d02717404edea428772444c27602e2f290461738
unknown <kennedyhillary6@gmail.com>
2023-04-13 23:39:25 +0100

    clean up

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -28,7 +28,7 @@ export class TasksController {
     @Query(ValidationPipe) filterDto: GetTaskFilterDto,
     @GetUser() user: User,
   ): Promise<Task[]> {
-    return this.taskService.getTasks(filterDto, user);
+    return this.taskService.getTasks(filterDto);
   }
 
   @Get(':id')
