40f286a234fa409531033e95ba8cd986681e79cf
unknown <kennedyhillary6@gmail.com>
2023-04-14 19:12:35 +0100

    Logger

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3500);
+  const port = 3500;
+  await app.listen(port);
+  logger.log(`Application started on port ${port}`);
 }
 bootstrap();
