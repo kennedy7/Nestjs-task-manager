23f057e557834ff9472e2c72b7bcee91c01f2fdf
unknown <kennedyhillary6@gmail.com>
2023-04-16 06:51:03 +0100

    AuthService loggger

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
@@ -7,6 +7,7 @@ import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from './jwt-payload.interface';
 @Injectable()
 export class AuthService {
+  private logger = new Logger('AuthService');
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
@@ -26,6 +27,9 @@ export class AuthService {
     }
     const payload: JwtPayload = { username };
     const token = await this.jwtService.sign(payload);
+    this.logger.debug(
+      `Generated JWT token with payload ${JSON.stringify(payload)} `,
+    );
     return { token };
   }
 }
