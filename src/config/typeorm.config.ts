557bef2aa4699b6e2d51913924870079282379d4
unknown <kennedyhillary6@gmail.com>
2023-04-16 11:09:17 +0100

    db configuration

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,11 +7,12 @@ const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
-  host: dbConfig.host,
-  port: dbConfig.port,
-  username: process.env.DB_USERNAME || dbConfig.username,
-  password: process.env.DB_PASSWORD || dbConfig.password,
-  database: process.env.DATABASE || dbConfig.database,
+  //RDS-- AMAAZON RELATION DATAbASE SYSTEM PARAMETERS
+  host: process.env.RDS_HOSTNAME || dbConfig.host,
+  port: process.env.RDS_PORT || dbConfig.port,
+  username: process.env.RDS_USERNAME || dbConfig.username,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  database: process.env.DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.ts'],
   synchronize: dbConfig.synchronize,
 };
